336b2f858ea443233aa947c901852827c9bb5f93
krystalgamer <callofdutypsp@gmail.com>
2019-01-06 23:13:15 +0000

    GIL CARALHO

---
+++ guess_painter/guess_painter.h
@@ -0,0 +1,12 @@
+/**
+ * @defgroup guess_painter guess_painter module
+ * Contains all the file multiplayer painter code
+ * @{
+ */
+
+/**
+ * @brief Creates the guess painter
+ */
+void create_guess_painter();
+
+/** @}*/
